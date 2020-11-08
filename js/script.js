75a299fb5f79609d7bf0761b45c2b1d2bb576e10
Jordan Kittle <shwackd@gmail.com>
2020-11-08 09:54:46 -0800

    Added payment info functionality

--- js/script.js
+++ js/script.js
@@ -11,10 +11,21 @@
 const name = document.getElementById('name').focus();
 const email = document.getElementById('email');
 const themeSelect = document.getElementById('design');
-const otherJobRoleInput = document.getElementById('other-title').style.display = 'none';
+const jobRoleInput = document.getElementById('title');
+const otherJobRoleInput = document.getElementById('other-title');
+otherJobRoleInput.style.display = 'none';
 const colorOptions = document.querySelectorAll('#color option');
 const colorSelect = document.querySelector('#color');
 hideColors();
+
+//Set even listener on job role and display other-title input if 'other' is selected
+jobRoleInput.addEventListener('change', e => {
+	if( event.target.value === 'other' ){
+		otherJobRoleInput.style.display = '';
+	} else {
+		otherJobRoleInput.style.display = 'none';
+	}
+});
 
 
 
@@ -95,6 +106,58 @@ document.querySelector('.activities').addEventListener('change', e => {
 		}
 	}
 	totalPriceLabel.innerHTML = `<h2>Total: $${costOfActivities}</h2>`;
-	totalPrice = costOfActivites;
+	totalPrice = costOfActivities;
 });
 
+
+/*Set payment default to credit card and hide other payment options
+ *Loop through all payment options and select by value to make sure credit card always selected even if options change
+ *"Select Payment" option is set be disabled
+*/
+const paymentSelect = document.querySelector('#payment');
+const paymentOptions = document.querySelectorAll('#payment option');
+for (let i = 0; i < paymentOptions.length; i++) {
+	if( paymentOptions[i].value === 'credit card' ){
+		paymentSelect.selectedIndex = i;
+	}
+	if( paymentOptions[i].value === 'select method' ){
+		paymentOptions[i].disabled = true;
+	}
+}
+const creditCard = document.querySelector('#credit-card');
+const paypal = document.querySelector('#paypal');
+const bitcoin = document.querySelector('#bitcoin');
+paypal.style.display = 'none';
+bitcoin.style.display = 'none';
+
+
+/*Set functions to handle payment option changes and set listener on payment select
+ *
+*/
+function payWithCreditCard(){
+	creditCard.style.display = '';
+	paypal.style.display = 'none';
+	bitcoin.style.display = 'none';
+}
+function payWithPayPal(){
+	paypal.style.display = '';
+	creditCard.style.display = 'none';
+	bitcoin.style.display = 'none';
+}
+function payWithBitcoin(){
+	bitcoin.style.display = '';
+	creditCard.style.display = 'none';
+	paypal.style.display = 'none';
+}
+paymentSelect.addEventListener('change', e => {
+	const paymentSelected = e.target.value;
+	if( paymentSelected === 'credit card' ){
+		payWithCreditCard()
+	} else if ( paymentSelected === 'paypal' ){
+		payWithPayPal();
+	} else if ( paymentSelected === 'bitcoin' ){
+		payWithBitcoin();
+	} else {
+
+	}
+});
